5e849ddd13b5c9632969f343c3c5418eecc3958f
mile <darimile@gmail.com>
2024-07-30 22:55:00 +0200

    add testimoniales

--- controles/testimonialController.js
+++ controles/testimonialController.js
@@ -1,6 +1,40 @@
 
 const guardarTestimonial = (req, res) => {
-    console.log(req.body);
+   
+   // Validar
+
+   const { nombre, correo, mensaje } = req.body;
+
+   const errores = [];
+
+   if(nombre.trim() ==='') {
+    errores.push({mensaje: 'El nombre esta vacio'});
+   }
+
+   if(correo.trim() ==='') {
+    errores.push({mensaje: 'El correo esta vacio'});
+   }
+
+   if(mensaje.trim() ==='') {
+    errores.push({mensaje: 'El mensaje esta vacio'});
+   }
+
+   if(errores.length>0) {
+
+
+    //Mostrar la vista con errores
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            errores,
+            nombre,
+            correo,
+            mensaje
+    })
+   }else {
+    // Almacenamiento en DB
+   }
+
+   
 }
 
 export {
--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use( (req, res, next) => {
 });
 
 // Agregar body parser para leer los datos del formulario
-
+app.use(express.urlencoded({extended: true}));
 
 // Definir la carpeta publica
 app.use(express.static('public'));
