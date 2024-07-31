b1f854813593e3a668a49f2b56e7a38ff13e4ca3
mile <darimile@gmail.com>
2024-07-31 17:18:53 +0200

    add testimoniales create

--- controles/testimonialController.js
+++ controles/testimonialController.js
@@ -1,5 +1,6 @@
+import {Testimonial} from '../models/Testimoniales.js';
 
-const guardarTestimonial = (req, res) => {
+const guardarTestimonial = async (req, res) => {
    
    // Validar
 
@@ -32,6 +33,18 @@ const guardarTestimonial = (req, res) => {
     })
    }else {
     // Almacenamiento en DB
+
+    try {
+        await Testimonial.create({
+            nombre,
+            correo,
+            mensaje
+        });
+
+        res.redirect('/testimoniales');
+    } catch (error) {
+        console.log(error);
+    }
    }
 
    
