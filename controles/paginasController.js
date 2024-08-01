239ec2f7bb5626379d191eca5ffa50b9ad5ca9a4
mile <darimile@gmail.com>
2024-08-01 20:35:32 +0200

    add dotenv

--- controles/paginasController.js
+++ controles/paginasController.js
@@ -2,10 +2,27 @@ import { Viaje} from '../models/Viajes.js';
 import { Testimonial } from '../models/Testimoniales.js';
 
 
-const paginaInicio = (req, res)=> { // req- lo que enviamos : res - lo que express nos responde
-    res.render('Inicio', {
-        pagina: 'Inicio'
-    });
+const paginaInicio = async (req, res)=> { // req- lo que enviamos : res - lo que express nos responde
+
+    // Consultar
+
+    const promiseDB = [];
+    promiseDB.push(Viaje.findAll({limit: 3}));
+    promiseDB.push(Testimonial.findAll({limit: 3}));
+    try {
+        const resultado = await Promise.all(promiseDB);       
+
+        res.render('Inicio', {
+            pagina: 'Inicio',
+            clase: 'home',
+            viajes: resultado[0],
+            testimoniales: resultado[1]
+        });
+    } catch (error) {
+        console.log(error)
+    }
+
+    
 }
 
 const paginaNosotros = (req, res)=> { 
